d456ea315b8868b10981a9e183813cab295c48ef
Sergey Goncharovр <doimpos@gmail.com>
2023-08-19 19:00:18 +0500

    Доработка первого блока

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -13,9 +13,32 @@ const Header = class Header {
             if (event.target.closest('.header__burger--body') || event.target.closest('.header__burger')) return;
             this.closeMobileMenu();
         }) 
+    }    
+    fixedHeader() {
+        document.addEventListener('DOMContentLoaded', () => {
+            const header = document.querySelector('.header');
+            const mainContent = document.querySelector('.first_block');
+            const headerHeight = header.offsetHeight;
+            let isHeaderShown = false;
+          
+            mainContent.style.paddingTop = `${headerHeight}px`;
+            const updateHeader = () => {
+              if (window.pageYOffset > 0 && !isHeaderShown) {
+                header.classList.add('isFixed');
+                isHeaderShown = true;
+              } else if (window.pageYOffset === 0 && isHeaderShown) {
+                header.classList.remove('isFixed');
+                isHeaderShown = false;
+              }
+            };
+          
+            window.addEventListener('scroll', updateHeader);
+            window.addEventListener('load', updateHeader);
+        });
     }
     init() {
         this.setEventListener();
+        this.fixedHeader();
     }
 }
 
