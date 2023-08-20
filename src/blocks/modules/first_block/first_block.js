00e4bf33fc1d65af1b5ff8529cff8d8ff0f3c98a
Sergey Goncharovр <doimpos@gmail.com>
2023-08-20 21:38:04 +0500

    Доработка 3д

--- src/blocks/modules/first_block/first_block.js
+++ src/blocks/modules/first_block/first_block.js
@@ -1,32 +1,38 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three-gltf-loader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const FirstBlock = class FirstBlock {
     constructor() {}
 
     animBlock() {
-        // const container = document.getElementById('model-three'); // Получите контейнер по ID
-
-        // const scene = new THREE.Scene();
-        // const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-        // const renderer = new THREE.WebGLRenderer();
-        // renderer.setSize(container.clientWidth, container.clientHeight);
-        // container.appendChild(renderer.domElement);
-
-        // const loader = new GLTFLoader();
-        // loader.load('./path/to/logos.glb', (gltf) => {
-        //     const model = gltf.scene;
-        //     scene.add(model);
-        // });
-
-        // camera.position.z = 5;
-
-        // const animate = () => {
-        //     requestAnimationFrame(animate);
-        //     renderer.render(scene, camera);
-        // };
-
-        // animate();
+        document.addEventListener('DOMContentLoaded', () => {
+            // Создаем сцену, камеру и рендерер
+            const scene = new THREE.Scene();
+            const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+            const renderer = new THREE.WebGLRenderer();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            document.getElementById('container').appendChild(renderer.domElement);
+        
+            // Создаем загрузчик GLTF
+            const loader = new GLTFLoader();
+            loader.load('files/logossssss.gltf', (gltf) => {
+                const model = gltf.scene;
+                scene.add(model);
+        
+                // Позиционируем камеру
+                camera.position.z = 5;
+        
+                // Создаем функцию для анимации
+                const animate = () => {
+                    requestAnimationFrame(animate);
+                    // Добавьте здесь код для анимации, если требуется
+                    renderer.render(scene, camera);
+                };
+        
+                model.scale.set(3, 3, 3); // Это увеличит модель вдвое по всем осям
+                animate();
+            });
+        });
     }
 
     init() {
