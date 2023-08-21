65e2d81d61f917f4b64e2141dda0485c3873aea3
Sergey Goncharov <sersh@MacBook-Pro-Sergey.local>
2023-08-21 14:26:53 +0500

    Шапка сайта

--- src/blocks/modules/first_block/first_block.js
+++ src/blocks/modules/first_block/first_block.js
@@ -12,26 +12,40 @@ const FirstBlock = class FirstBlock {
             const renderer = new THREE.WebGLRenderer();
             renderer.setSize(window.innerWidth, window.innerHeight);
             document.getElementById('container').appendChild(renderer.domElement);
-        
+            window.addEventListener('resize', () => {
+                const newWidth = window.innerWidth;
+                const newHeight = window.innerHeight;
+                camera.aspect = newWidth / newHeight;
+                camera.updateProjectionMatrix();
+                renderer.setSize(newWidth, newHeight);
+            });
             // Создаем загрузчик GLTF
             const loader = new GLTFLoader();
-            loader.load('files/logossssss.gltf', (gltf) => {
+            loader.load('files/scene.gltf', (gltf) => {
                 const model = gltf.scene;
                 scene.add(model);
         
                 // Позиционируем камеру
-                camera.position.z = 5;
+                model.rotation.y += 0.01;
+                camera.position.x = 0;
+                camera.position.y = 1.5;
+                camera.position.z = 3.4;
         
-                // Создаем функцию для анимации
-                const animate = () => {
-                    requestAnimationFrame(animate);
-                    // Добавьте здесь код для анимации, если требуется
-                    renderer.render(scene, camera);
-                };
-        
-                model.scale.set(3, 3, 3); // Это увеличит модель вдвое по всем осям
-                animate();
+                model.scale.set(2, 2, 2); // Это увеличит модель вдвое по всем осям
             });
+            
+            // Создаем функцию для анимации
+            const animate = () => {
+                requestAnimationFrame(animate);
+                
+                // Добавьте код анимации здесь
+                // model.rotation.x += 0.01;
+                // model.rotation.y += 0.01;
+                
+                renderer.render(scene, camera);
+            };
+            
+            animate();
         });
     }
 
