15de416f6e7e31fb5bc6aeee3c4fd26a1e62598d
Sergey Goncharovр <doimpos@gmail.com>
2023-08-22 23:48:21 +0500

    Доработка 3д

--- src/blocks/modules/first_block/first_block.js
+++ src/blocks/modules/first_block/first_block.js
@@ -8,7 +8,9 @@ const FirstBlock = class FirstBlock {
         document.addEventListener('DOMContentLoaded', () => {
             const scene = new THREE.Scene();
             const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-            const renderer = new THREE.WebGLRenderer();
+            const renderer = new THREE.WebGLRenderer({
+                antialias: true
+            });
             renderer.setSize(window.innerWidth, window.innerHeight);
             document.getElementById('container').appendChild(renderer.domElement);
             window.addEventListener('resize', () => {
