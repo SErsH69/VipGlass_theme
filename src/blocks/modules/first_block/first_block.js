59c0b94be6970a1079ce2ed67eae6bbcc11b2d20
Sergey Goncharovр <doimpos@gmail.com>
2023-08-24 07:53:03 +0500

    Доработка 3д

--- src/blocks/modules/first_block/first_block.js
+++ src/blocks/modules/first_block/first_block.js
@@ -1,63 +1,63 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+// import * as THREE from 'three';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const FirstBlock = class FirstBlock {
     constructor() {}
 
     animBlock() {
-        document.addEventListener('DOMContentLoaded', () => {
-            const scene = new THREE.Scene();
-            const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-            const renderer = new THREE.WebGLRenderer({
-                antialias: true
-            });
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            document.getElementById('container').appendChild(renderer.domElement);
-            window.addEventListener('resize', () => {
-                const newWidth = window.innerWidth;
-                const newHeight = window.innerHeight;
-                camera.aspect = newWidth / newHeight;
-                camera.updateProjectionMatrix();
-                renderer.setSize(newWidth, newHeight);
-            });
+        // document.addEventListener('DOMContentLoaded', () => {
+        //     const scene = new THREE.Scene();
+        //     const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+        //     const renderer = new THREE.WebGLRenderer({
+        //         antialias: true
+        //     });
+        //     renderer.setSize(window.innerWidth, window.innerHeight);
+        //     document.getElementById('container').appendChild(renderer.domElement);
+        //     window.addEventListener('resize', () => {
+        //         const newWidth = window.innerWidth;
+        //         const newHeight = window.innerHeight;
+        //         camera.aspect = newWidth / newHeight;
+        //         camera.updateProjectionMatrix();
+        //         renderer.setSize(newWidth, newHeight);
+        //     });
             
-            const loader = new GLTFLoader();
-            let model;
-            let mixer; // Добавляем переменную для микшера анимаций
+        //     const loader = new GLTFLoader();
+        //     let model;
+        //     let mixer; // Добавляем переменную для микшера анимаций
             
-            loader.load('files/scene.gltf', (gltf) => {
-                model = gltf.scene;
-                scene.add(model);
+        //     loader.load('files/scene.gltf', (gltf) => {
+        //         model = gltf.scene;
+        //         scene.add(model);
         
-                model.rotation.y += 0.01;
-                camera.position.x = 0;
-                camera.position.y = 0.9;
-                camera.position.z = 2;
+        //         model.rotation.y += 0.01;
+        //         camera.position.x = 0;
+        //         camera.position.y = 0.9;
+        //         camera.position.z = 2;
         
-                // model.scale.set(2, 2, 2);
+        //         // model.scale.set(2, 2, 2);
                 
-                // Извлекаем анимацию из загруженной модели
-                mixer = new THREE.AnimationMixer(model);
-                const animations = gltf.animations;
-                if (animations && animations.length) {
-                    for (let i = 0; i < animations.length; i++) {
-                        mixer.clipAction(animations[i]).play(); // Запускаем анимацию
-                    }
-                }
+        //         // Извлекаем анимацию из загруженной модели
+        //         mixer = new THREE.AnimationMixer(model);
+        //         const animations = gltf.animations;
+        //         if (animations && animations.length) {
+        //             for (let i = 0; i < animations.length; i++) {
+        //                 mixer.clipAction(animations[i]).play(); // Запускаем анимацию
+        //             }
+        //         }
                 
-                animate();
-            });
+        //         animate();
+        //     });
             
-            const animate = () => {
-                requestAnimationFrame(animate);
+        //     const animate = () => {
+        //         requestAnimationFrame(animate);
                 
-                if (mixer) {
-                    mixer.update(0.01); // Обновляем состояние анимаций
-                }
+        //         if (mixer) {
+        //             mixer.update(0.01); // Обновляем состояние анимаций
+        //         }
                 
-                renderer.render(scene, camera);
-            };
-        });
+        //         renderer.render(scene, camera);
+        //     };
+        // });
     }
 
     init() {
