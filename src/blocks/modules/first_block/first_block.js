322e51def3dbbad57b5b4a3078002694890c27bb
Sergey Goncharovр <doimpos@gmail.com>
2023-08-25 23:33:12 +0500

    Доработка блоков

--- src/blocks/modules/first_block/first_block.js
+++ src/blocks/modules/first_block/first_block.js
@@ -18,7 +18,7 @@ const FirstBlock = class FirstBlock {
         const near = 0.1;
         const far = 100;
         const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        camera.position.set(0.048613096583193814, 0.9789306422842851, 3.8393410178578407);
+        camera.position.set(0.048613096583193814, 0.5789306422842851, 3.8393410178578407);
         camera.rotation.set(-0.05018101139354198, -0.00015137332018180972, -0.000007602448675014163);
         let globalModel;
 
